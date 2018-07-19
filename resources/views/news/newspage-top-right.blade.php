8d138b82d7da36ccb4a6df23dd7d478d1f242c69
square <815566717@qq.com>
2018-07-20 00:53:31 +0800

    添加了新闻页面

---
+++ resources/views/news/newspage-top-right.blade.php
@@ -0,0 +1,38 @@
+<div class="mdui-col-md-12">
+    <li class="mdui-list-item">
+        <i class="mdui-list-item-icon mdui-icon material-icons">send</i>
+        <div class="mdui-list-item-content">newsTitle1</div>
+    </li>
+    <li class="mdui-list-item">
+        <i class="mdui-list-item-icon mdui-icon material-icons">send</i>
+        <div class="mdui-list-item-content">newsTitle2</div>
+    </li>
+    <li class="mdui-list-item">
+        <i class="mdui-list-item-icon mdui-icon material-icons">send</i>
+        <div class="mdui-list-item-content">newsTitle3</div>
+    </li>
+    <li class="mdui-list-item">
+        <i class="mdui-list-item-icon mdui-icon material-icons">send</i>
+        <div class="mdui-list-item-content">newsTitle4</div>
+    </li>
+    <li class="mdui-list-item">
+        <i class="mdui-list-item-icon mdui-icon material-icons">send</i>
+        <div class="mdui-list-item-content">newsTitle5</div>
+    </li>
+    <li class="mdui-list-item">
+        <i class="mdui-list-item-icon mdui-icon material-icons">send</i>
+        <div class="mdui-list-item-content">newsTitle6</div>
+    </li>
+    <li class="mdui-list-item">
+        <i class="mdui-list-item-icon mdui-icon material-icons">send</i>
+        <div class="mdui-list-item-content">newsTitle7</div>
+    </li>
+    <li class="mdui-list-item">
+        <i class="mdui-list-item-icon mdui-icon material-icons">send</i>
+        <div class="mdui-list-item-content">newsTitle8</div>
+    </li>
+    <li class="mdui-list-item">
+        <i class="mdui-list-item-icon mdui-icon material-icons">send</i>
+        <div class="mdui-list-item-content">newsTitle9</div>
+    </li>
+</div>
