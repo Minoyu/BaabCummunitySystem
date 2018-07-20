26d88eba151ff6dea01247e7c3f00e120cebedb2
Mino_Yu <546354337@qq.com>
2018-07-20 16:12:16 +0800

    改变部分目录结构 优化css

--- resources/views/news-sec.blade.php
+++ resources/views/news-sec.blade.php
@@ -7,10 +7,10 @@
 @section('content')
     {{--二级新闻页主列表--}}
     <div class="mdui-col-md-9 mdui-col-xs-12">
-        @include('news.news-sec-lists')
+        @include('news-sec.lists')
     </div>
     {{--侧边栏，板块切换--}}
     <div class="mdui-col-md-3 mdui-hidden-sm-down">
-        @include('news.news-sec-side-clumn')
+        @include('news-sec.side-clumn')
     </div>
 @endsection
---
+++ resources/views/news-sec/lists.blade.php
@@ -0,0 +1,15 @@
+<div class="mdui-col-md-12">
+    <h2 class="title-with-bottom-border">
+        热点新闻
+    </h2>
+    <div class="news-sec-list">
+        <div class="mdui-hoverable">
+            <div class="news-sec-list-item">
+                <img class="news-sec-list-img" src="http://via.placeholder.com/150x150">
+            </div>
+            <div class="news-sec-list-pattern">
+                newsTitle
+            </div>
+        </div>
+    </div>
+</div>
